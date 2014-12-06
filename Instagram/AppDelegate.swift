ef9f86f6b7b62aa22eee33282606e49e193d5b1c
Rustan Corpuz <rustanacexd@gmail.com>
2014-12-06 22:43:03 +0800

    Initial Commit

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -15,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        Parse.setApplicationId("JiloBGPrFNoTdq3TOptL8qsMLGocBP55vYjkXIZr", clientKey: "21mQASuCvkzq57m07C8rzO51qqpCoK4MqyMFFlld")
         // Override point for customization after application launch.
         return true
     }
--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -8,18 +8,142 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    
+    var activityIndicator:UIActivityIndicatorView!
+    var error = ""
+    //    var signupActive = true
+    
+    @IBAction func signUp(sender: AnyObject) {
+        
+        if validateTextFields() {
+            signUp()
+        }
+        
+    }
+    
+    @IBAction func loginButtonTapped(sender: AnyObject) {
+        if validateTextFields() {
+            logIn()
+        }
+    }
+    
+    
+    //Overrides
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        if PFUser.currentUser() != nil {
+            self.performSegueWithIdentifier("jumpToUserTable", sender: self)
+        }
+    }
+    
+    
 
-
+    //Parse Helper Methods
+    
+    func signUp() {
+        var user = PFUser()
+        user.username =  username.text
+        user.password = password.text
+        
+        displayActivityIndicator()
+        
+        user.signUpInBackgroundWithBlock {
+            (succeeded: Bool!, signupError: NSError!) -> Void in
+            
+            self.activityIndicator.stopAnimating()
+            UIApplication.sharedApplication().endIgnoringInteractionEvents()
+            
+            if signupError == nil {
+                self.displayAlert(title: "Successfully Signed up", alertMessage: "")
+            } else {
+                if let errorString = signupError.userInfo?["error"] as? NSString {
+                    self.error = errorString
+                }else{
+                    self.error = "Something is wrong"
+                }
+                // Show the errorString somewhere and let the user try again.
+                self.displayAlert(title: "Could not sign up", alertMessage: self.error)
+            }
+        }
+    }
+    
+    func logIn(){
+        
+        displayActivityIndicator()
+        
+        PFUser.logInWithUsernameInBackground(username.text, password:password.text) {
+            (user: PFUser!, loginError: NSError!) -> Void in
+            
+            self.activityIndicator.stopAnimating()
+            UIApplication.sharedApplication().endIgnoringInteractionEvents()
+            
+            if user != nil {
+                // Do stuff after successful login.
+                self.displayAlert(title: "Successfully logged in", alertMessage: "")
+            } else {
+                if let errorString = loginError.userInfo?["error"] as? NSString {
+                    self.error = errorString
+                }else{
+                    self.error = "Something is wrong"
+                }
+                // Show the errorString somewhere and let the user try again.
+                self.displayAlert(title: "Could not log in", alertMessage: self.error)
+            }
+        }
+    }
+    
+    //Custom Helper Methods
+    
+    func displayAlert(#title:String, alertMessage:String){
+        
+        var alert = UIAlertController(title: title, message: alertMessage, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: {
+            action in
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }))
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func displayActivityIndicator(){
+        
+        activityIndicator = UIActivityIndicatorView(frame: CGRectMake(0, 0, 100, 100))
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+        view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+        UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+    }
+    
+    func validateTextFields() -> Bool{
+        if username.text == "" || password.text == "" {
+            error = "Please enter username and password"
+            displayAlert(title: "Error in form", alertMessage: error)
+            return false
+        }else{
+            return true
+        }
+    }
+    
+    
 }
 
